aedf7e19a6d3ba3ed03b294838a9397cbaceb7d7
Filip Hracek <filip.hracek@gmail.com>
2024-06-17 22:11:03 +0200

    Initial

---
+++ README.md
@@ -0,0 +1,18 @@
+This is a tiny project that uses 
+[Electron](https://www.electronjs.org/)
+to wrap the 
+[Construct 3](https://www.construct.net/en)
+(game engine) web app into a standalone application.
+
+Why? This is so that the kid can use the web-based tool without me having to open up
+the whole internet for them. 
+I'm using a family safety tool that gives the kid only a limited amount of time for some apps,
+namely the browser.
+But I also want the kid to have unlimited time for creativity.
+Thus, I need Construct 3 to be a standalone app.
+
+**Full disclosure:** 
+Absolutely no warranty, no support, no documentation, no time to even explain this project.
+I'm making this open source on the off chance that it's useful for someone else out there,
+but if you hit a snag, I'm afraid you're on your own.
+
---
+++ index.js
@@ -0,0 +1,64 @@
+const { app, BrowserWindow, session } = require('electron');
+
+function createWindow() {
+  // Create the browser window.
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: false, // Disable Node integration for security
+      contextIsolation: true  // Enable context isolation for security
+    }
+  });
+
+  // Load the website
+  win.loadURL('https://editor.construct.net/');
+
+  // Open the DevTools (optional)
+  // win.webContents.openDevTools();
+
+  // Intercept and filter network requests
+  const filter = {
+    urls: ['*://*/*']
+  };
+
+  session.defaultSession.webRequest.onBeforeRequest(filter, (details, callback) => {
+    const { url } = details;
+    if (url.startsWith('https://editor.construct.net/')) {
+      callback({ cancel: false });
+    } else {
+      callback({ cancel: true });
+    }
+  });
+
+  // Prevent navigation to other domains
+  win.webContents.on('will-navigate', (event, url) => {
+    if (!url.startsWith('https://editor.construct.net/')) {
+      event.preventDefault();
+    }
+  });
+
+  win.webContents.on('new-window', (event, url) => {
+    if (!url.startsWith('https://editor.construct.net/')) {
+      event.preventDefault();
+    }
+  });
+}
+
+// This method will be called when Electron has finished initialization.
+app.whenReady().then(createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications to stay open until the user explicitly quits.
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // On macOS it is common to re-create a window when the dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
